752d985697aee5cde2f4f4417928cc8ce17684db
Joaquin Pereyra <joaquinpereyra98@users.noreply.github.com>
2025-08-14 18:28:11 -0300

    FIX reset-storage

--- tools/reset-storage.mjs
+++ tools/reset-storage.mjs
@@ -4,19 +4,25 @@ import path from "path";
 
 const TARGET_PATH = "./storage/assets";
 
+if (!fs.existsSync(TARGET_PATH)) {
+  fs.mkdirSync(TARGET_PATH, { recursive: true });
+}
+
 for (const file of fs.readdirSync(TARGET_PATH)) {
   const filePath = path.join(TARGET_PATH, file);
   const stat = fs.lstatSync(filePath);
-  stat.isDirectory() ? fs.rmSync(filePath, { recursive: true, force: true }) : fs.unlinkSync(filePath);
+  stat.isDirectory()
+    ? fs.rmSync(filePath, { recursive: true, force: true })
+    : fs.unlinkSync(filePath);
 }
 
 [...CONSTANTS.ACTOR_PACKS, ...CONSTANTS.ITEM_PACKS].forEach(pack => {
-  const packPath = path.join(path.join(TARGET_PATH, "portraits"), pack);
+  const packPath = path.join(TARGET_PATH, "portraits", pack);
   fs.mkdirSync(packPath, { recursive: true });
 });
 
 CONSTANTS.ACTOR_PACKS.forEach(pack => {
-  const packPath = path.join(path.join(TARGET_PATH, "tokens"), pack);
+  const packPath = path.join(TARGET_PATH, "tokens", pack);
   fs.mkdirSync(packPath, { recursive: true });
 });
 
